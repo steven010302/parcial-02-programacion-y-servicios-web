ceec57abd8cf5e56452be93f193c6d3b8403cdde
steven010302 <is.pena01@ciaf.edu.co>
2025-10-19 21:19:12 -0500

    Update photo URLs for user profiles

--- Express/Data/Usuarios.js
+++ Express/Data/Usuarios.js
@@ -4,70 +4,70 @@ let Usuarios  = [{
   "email": "pargo0@squarespace.com",
   "Address": "048 Lighthouse Bay Crossing",
   "age": 42,
-  "photoURL": "http://dummyimage.com/165x100.png/5fa2dd/ffffff"
+  "photoURL": "https://i.pravatar.cc/150?img=1"
 }, {
   "User_ID": 2,
   "name": "Gisela Hadland",
   "email": "ghadland1@wisc.edu",
   "Address": "632 John Wall Way",
   "age": 38,
-  "photoURL": "http://dummyimage.com/114x100.png/dddddd/000000"
+  "photoURL": "https://i.pravatar.cc/150?img=2"
 }, {
   "User_ID": 3,
   "name": "Oren Duffill",
   "email": "oduffill2@cafepress.com",
   "Address": "66921 Holmberg Crossing",
   "age": 47,
-  "photoURL": "http://dummyimage.com/195x100.png/ff4444/ffffff"
+  "photoURL": "https://i.pravatar.cc/150?img=3"
 }, {
   "User_ID": 4,
   "name": "Francois Barson",
   "email": "fbarson3@sfgate.com",
   "Address": "9661 8th Circle",
   "age": 48,
-  "photoURL": "http://dummyimage.com/241x100.png/5fa2dd/ffffff"
+  "photoURL": "https://i.pravatar.cc/150?img=4"
 }, {
   "User_ID": 5,
   "name": "Kelwin Stainfield",
   "email": "kstainfield4@mit.edu",
   "Address": "7279 Rigney Drive",
   "age": 18,
-  "photoURL": "http://dummyimage.com/247x100.png/cc0000/ffffff"
+  "photoURL": "https://i.pravatar.cc/150?img=5"
 }, {
   "User_ID": 6,
   "name": "Bendite Seller",
   "email": "bseller5@purevolume.com",
   "Address": "63 Hollow RUser_IDge Pass",
   "age": 31,
-  "photoURL": "http://dummyimage.com/106x100.png/cc0000/ffffff"
+  "photoURL": "https://i.pravatar.cc/150?img=6"
 }, {
   "User_ID": 7,
   "name": "Pier Conklin",
   "email": "pconklin6@rambler.ru",
   "Address": "0 Fairfield Road",
   "age": 21,
-  "photoURL": "http://dummyimage.com/150x100.png/cc0000/ffffff"
+  "photoURL": "https://i.pravatar.cc/150?img=7"
 }, {
   "User_ID": 8,
   "name": "Byrann Pennycuick",
   "email": "bpennycuick7@hud.gov",
   "Address": "50 Carioca Street",
   "age": 45,
-  "photoURL": "http://dummyimage.com/145x100.png/5fa2dd/ffffff"
+  "photoURL": "https://i.pravatar.cc/150?img=8"
 }, {
   "User_ID": 9,
   "name": "Constancy Killby",
   "email": "ckillby8@infoseek.co.jp",
   "Address": "5355 Melvin Avenue",
   "age": 29,
-  "photoURL": "http://dummyimage.com/195x100.png/cc0000/ffffff"
+  "photoURL": "https://i.pravatar.cc/150?img=9"
 }, {
   "User_ID": 10,
   "name": "Jillene Crossan",
   "email": "jcrossan9@ehow.com",
   "Address": "81 Longview Park",
   "age": 42,
-  "photoURL": "http://dummyimage.com/217x100.png/ff4444/ffffff"
+  "photoURL": "https://i.pravatar.cc/150?img=10"
 }]
 
 module.exports = Usuarios;
